ac79cd603b63178e6ab531b74079421eccedb17d
ArihantRJain <arihanttrue@gmail.com>
2018-03-21 11:05:42 +0530

    Adding a route PUT /bid to update a Bid Value. Changing /add_bid to /bid

--- app/controllers/bidController.js
+++ app/controllers/bidController.js
@@ -15,7 +15,7 @@ function buildRowPromises(requestObject) {
   return promises;
 }
 
-function vendorPosition(tenderId) {
+function updatePosition(tenderId, BidId, message, res) {
 
 	Bid.findAll({where: {tenderId: tenderId}, order: ['value'], raw: true}).then(function(bids) {
 
@@ -33,8 +33,27 @@ function vendorPosition(tenderId) {
 
 		bidIds.forEach((bidId, i) => bidIdPositions[bidId] = positions[i]);
 
-		Promise.all(buildRowPromises(bidIdPositions))
+		Promise.all(buildRowPromises(bidIdPositions)).then(function(updatedBids) {
+			Bid.findOne({where: {id: BidId}}).then(function(updatedBid) {
 
+				temp = common.ResponseFormat(200, '', []);
+
+				if (updatedBid) {
+					if (message == 'update') {
+
+						updatedBid.decrement(['attemptsRemaining'], {by: 1});
+
+						temp.message = 'Successfully updated Bid ' + updatedBid.id;
+					}
+					else {
+						temp.message = 'Successfully created Bid ' + updatedBid.id;
+					}
+				}
+
+				res.status(temp.status)
+					.json(temp);
+			});
+		});
 	});
 }
 
@@ -51,19 +70,16 @@ exports.add_bid = function(req, res) {
 
 		Bid.create(bidInfo).then(function(newBid){
 
-			temp = common.ResponseFormat(200, '', []);
 			if(newBid) {
-				vendorPosition(req.body.tenderId, req.body.value);
-				temp.message = 'Successfully created the Bid!';
+				updatePosition(req.body.tenderId, newBid.id, 'create', res);
 			}
 			else {
-				temp.message = 'Unable to create the Bid!';
+
+				temp = common.ResponseFormat(200, 'Unable to create the Bid!', []);
+				res.status(temp.status)
+					.json(temp);
 			}
-
-			res.status(temp.status)
-				.json(temp);
 		})
-
 	}
 	else {   
 
@@ -71,4 +87,35 @@ exports.add_bid = function(req, res) {
         res.status(temp.status)
             .json(temp);
 	}
+}
+
+exports.update_bid = function(req, res) {
+
+	if (req.body.value && req.body.bidId) {
+
+		Bid.update({value: req.body.value}, {where: {id: req.body.bidId}}).then(function(updated) {
+
+			if(updated) {
+
+				Bid.findById(req.body.bidId, {attributes: ['id', 'tenderId']}).then(function(updatedBid) {
+
+					if (updatedBid) {
+						updatePosition(updatedBid.tenderId, updatedBid.id, 'update', res);
+					}
+				})
+			}
+			else {
+
+				temp = common.ResponseFormat(200, 'Unable to update the Bid ' + req.body.bidId , []);
+				res.status(temp.status)
+					.json(temp);
+			}
+		})
+	}
+	else {   
+
+        temp = common.ResponseFormat(422, 'Missing Parameters!', req.body);
+        res.status(temp.status)
+            .json(temp);
+	} 
 }
--- app/routes/bids.js
+++ app/routes/bids.js
@@ -4,6 +4,7 @@ var bidRouter = express.Router();
 var bids = require('../controllers/bidController.js');
 var common = require('../common/common.js');
 
-bidRouter.post('/add_bid', common.isLoggedIn, bids.add_bid);
+bidRouter.post('/bid', common.isLoggedIn, bids.add_bid);
+bidRouter.put('/bid', common.isLoggedIn, bids.update_bid);
 
 module.exports = bidRouter;
