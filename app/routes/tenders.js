be6c6c76ee3b6928f16a8db1529383b885dcc69a
ArihantRJain <arihanttrue@gmail.com>
2018-03-23 14:59:31 +0530

    Updating the route to GET /tenders_main_category to /potential_tenders. This route will be able to retrieve tenders which the Vendor can bid on excluding tenders that the Vendor has already bid on

--- app/routes/tenders.js
+++ app/routes/tenders.js
@@ -7,7 +7,7 @@ var common = require('../common/common.js');
 tenderRouter.post('/tender', common.isLoggedIn, tenders.add_tender)
 tenderRouter.get('/tender/:tenderId', common.isLoggedIn, tenders.get_tender)
 tenderRouter.get('/client_tenders', common.isLoggedIn, tenders.get_client_tenders)
-tenderRouter.get('/tenders_main_category', common.isLoggedIn, tenders.get_main_category_tenders)
+tenderRouter.get('/potential_tenders', common.isLoggedIn, tenders.get_potential_tenders)
 tenderRouter.get('/tender_bids/:tenderId', common.isLoggedIn, tenders.get_all_bids)
 
 module.exports = tenderRouter;
