92ae0054b458bc66fb5cce781d0d7118ce8d84a9
Arihant Jain <arihanttrue@gmail.com>
2018-03-28 22:06:45 +0530

    Adding a route GET /bid/:bidId. Retrieves the details for the selected bid along with the Tender Details

--- app/routes/bids.js
+++ app/routes/bids.js
@@ -7,5 +7,6 @@ var common = require('../common/common.js');
 bidRouter.post('/bid', common.isLoggedIn, bids.add_bid);
 bidRouter.put('/bid', common.isLoggedIn, bids.update_bid);
 bidRouter.get('/bids/:status', common.isLoggedIn, bids.get_bids);
+bidRouter.get('/bid/:bidId', common.isLoggedIn, bids.get_bid);
 
 module.exports = bidRouter;
